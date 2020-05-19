13148425ab516addee876f1a502013cf92b68921
Damini Sharma <damini.sharma23@gmail.com>
2020-05-19 14:21:35 -0500

    exploring datasets to create recidivism flag

--- files/query_db.py
+++ files/query_db.py
@@ -12,7 +12,11 @@ def query_db(conn):
 
     try:
         c = conn.cursor()
-        sql = 'SELECT * FROM OFNT3CE1 limit 1000;'
+        sql = '''SELECT * FROM INMT4AA1
+                LEFT JOIN OFNT3CE1
+                    ON OFNT3CE1.OFFENDER_NC_DOC_ID_NUMBER = INMT4AA1.INMATE_DOC_NUMBER
+                limit 10500;'''
+        sql = '''SELECT * FROM OFNT3CE1 WHERE OFFENDER_NC_DOC_ID_NUMBER = '0130556';'''
         df = pd.read_sql_query(sql,conn)
         #c.execute(sql)
 
@@ -22,6 +26,20 @@ def query_db(conn):
 
     return(df)
 
+def query_db_notebook(conn,sql):
+    '''
+    Function to query database and save results as pandas dataframe for further analysis and cleaning
+    '''
+
+    try:
+        c = conn.cursor()
+        df = pd.read_sql_query(sql,conn)
+        #c.execute(sql)
+    except Error as e:
+        print(e)
+
+    return(df)
+    
 # For testing
 # def main():
 
