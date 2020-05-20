c72cfaa2f2cf1f2f0ae3c28aa9e251e07893be49
Hana Passen <passen@uchicago.edu>
2020-05-20 00:06:18 -0500

    cleaned up files we didn't need, started trying to code the dummies for policies

---
+++ exploratory_analysis/distance_policy_wrangle.py
@@ -0,0 +1,11 @@
+'''
+Christi Liongson, Hana Passen, Charmaine Runes, Damini Sharma
+
+Module to clean and wrangle the data from datasets on prison COVID-19 related
+social distancing measures into a series of dummy variables at the state level.
+'''
+
+def transform_easy_cols(df, features, new_col_names):
+    '''
+    '''
+    
